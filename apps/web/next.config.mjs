5f10e862e56a0b74bb8766b94249be3c6f8cfd22
Panem <titoluccao@gmail.com>
2023-10-17 17:50:58 -0300

    fix imports

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,12 +1,11 @@
 // Importing env files here to validate on build
 import "./src/env.mjs";
-import "@promofindr/auth/env.mjs";
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
   /** Enables hot reloading for local packages without a build step */
-  transpilePackages: ["@promofindr/api", "@promofindr/auth", "@promofindr/db"],
+  transpilePackages: ["@promofindr/api", "@promofindr/db"],
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
